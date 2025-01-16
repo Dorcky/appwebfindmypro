8360751b1901060f22d70d49baedfda8d5cdea80
Dorcky <161785219+Dorcky@users.noreply.github.com>
2025-01-16 11:33:23 -0500

    login translation

--- src/components/loginscreen/LoginScreen.js
+++ src/components/loginscreen/LoginScreen.js
@@ -6,17 +6,19 @@ import { useNavigate } from "react-router-dom";
 import logo from "../../assets/images/logo.png";
 import "./LoginScreen.css";
 import PropTypes from "prop-types";
+import { useTranslation } from 'react-i18next';
 
 const LoginScreen = ({ onClose, onSignupClick }) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
   const navigate = useNavigate();
+  const { t } = useTranslation();
 
   const login = async (e) => {
     e.preventDefault();
     if (email === "" || password === "") {
-      setErrorMessage("Veuillez remplir tous les champs.");
+      setErrorMessage(t('login.errors.emptyFields')); // Traduction du message d'erreur
       return;
     }
 
@@ -36,13 +38,13 @@ const LoginScreen = ({ onClose, onSignupClick }) => {
         } else if (role === "provider") {
           navigate(`/my-provider-profile/${user.uid}`); // Redirection sans fermer le modal
         } else {
-          setErrorMessage("Rôle non reconnu.");
+          setErrorMessage(t('login.errors.unknownRole')); // Traduction du message d'erreur
         }
       } else {
-        setErrorMessage("Utilisateur non trouvé.");
+        setErrorMessage(t('login.errors.userNotFound')); // Traduction du message d'erreur
       }
     } catch (error) {
-      setErrorMessage(error.message || "Une erreur inconnue est survenue.");
+      setErrorMessage(t('login.errors.unknownError')); // Traduction du message d'erreur
     }
   };
 
@@ -50,20 +52,20 @@ const LoginScreen = ({ onClose, onSignupClick }) => {
     <div className="container">
       <div className="flex flex-col items-center align-center">
         <img src={logo} alt="Icon" className="icon" />
-        <h1 className="title">Se connecter</h1>
+        <h1 className="title">{t('login.title')}</h1>
 
         <form onSubmit={login}>
           <input
             type="email"
             className="input"
-            placeholder="Email"
+            placeholder={t('login.email')}
             value={email}
             onChange={(e) => setEmail(e.target.value)}
           />
           <input
             type="password"
             className="input"
-            placeholder="Mot de passe"
+            placeholder={t('login.password')}
             value={password}
             onChange={(e) => setPassword(e.target.value)}
           />
@@ -71,12 +73,12 @@ const LoginScreen = ({ onClose, onSignupClick }) => {
           {errorMessage && <p className="error-text">{errorMessage}</p>}
 
           <button type="submit" className="py-3.5 px-5 bg-btn_primary text-white mb-2 button">
-            Se connecter
+            {t('login.login')}
           </button>
         </form>
 
         <button onClick={onSignupClick} className="sign-up-text py-3.5 px-5">
-          Pas de compte ? S&lsquo;inscrire
+          {t('login.noAccount')} <span className="underline">{t('login.signup')}</span>
         </button>
       </div>
     </div>
@@ -86,6 +88,7 @@ const LoginScreen = ({ onClose, onSignupClick }) => {
 LoginScreen.propTypes = {
   onClose: PropTypes.func.isRequired,
   onSignupClick: PropTypes.func.isRequired,
+  t: PropTypes.func.isRequired,
 };
 
 export default LoginScreen;
