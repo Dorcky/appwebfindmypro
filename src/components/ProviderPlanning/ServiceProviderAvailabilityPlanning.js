6239f2f6b10119d4deffe3de3c450f0e818f555e
Dorcky <161785219+Dorcky@users.noreply.github.com>
2025-01-15 19:17:16 -0500

    Your commitwell located navbar in provider-planning

--- src/components/ProviderPlanning/ServiceProviderAvailabilityPlanning.js
+++ src/components/ProviderPlanning/ServiceProviderAvailabilityPlanning.js
@@ -144,8 +144,8 @@ const ServiceProviderAvailabilityPlanning = () => {
   if (error) return <div className="p-4 text-red-500">Erreur: {error}</div>;
 
   return (
-    <div className="max-w-4xl mx-auto p-4 mt-40">
-      <h2 className="text-2xl font-bold mb-6">Planifier une disponibilité</h2>
+    <div className="max-w-4xl mx-auto p-4 ">
+      <h2 className="text-2xl font-bold mb-6 mt-20">Planifier une disponibilité</h2>
 
       <div className="mb-8 p-4 bg-white rounded shadow">
         <div className="grid gap-4 mb-4">
